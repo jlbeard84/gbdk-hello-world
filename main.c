4fd069b54d6974a9135dbf9f41f99dfb6f1075bb
Josh Beard <josh@jlbeard.com>
2021-03-03 23:12:32 -0500

    Refactored to make player change direction

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <gb/gb.h>
+#include <string.h>
 #include "alpha.c"
 #include "hello_world.c"
 #include "blank_screen.c"
 #include "player_sprite.c"
+#include "player_direction.c"
 
 void main();
 void init();
@@ -11,6 +13,7 @@ void updateSwitches();
 void drawPlayer();
 
 UINT8 playerPosition[2];
+enum PlayerDirection playerDirection = Down;
 
 void main() {
     init();
@@ -28,11 +31,12 @@ void init() {
     set_bkg_tiles(0, 0, 20, 18, blankScreen);
     set_bkg_tiles(5, 17, 11, 1, helloWorld);
 
-    set_sprite_data(0, 4, player_sprite);
-    set_sprite_tile(0, 0);
-    set_sprite_tile(1, 1);
-    set_sprite_tile(2, 2);
-    set_sprite_tile(3, 3);
+    set_sprite_data(0, 16, player_sprite);
+
+    set_sprite_tile(0, player_down_tile[0]);
+    set_sprite_tile(1, player_down_tile[1]);
+    set_sprite_tile(2, player_down_tile[2]);
+    set_sprite_tile(3, player_down_tile[3]);
 
     playerPosition[0] = 80;
     playerPosition[1] = 72;
@@ -49,25 +53,52 @@ void updateSwitches() {
 void checkInput() {
     if (joypad() & J_UP && playerPosition[1] > 16) {
 		playerPosition[1]--;
+        playerDirection = Up;
 	}
 
 	if (joypad() & J_DOWN && playerPosition[1] < 136) {
 		playerPosition[1]++;
+        playerDirection = Down;
 	}
 
 	if (joypad() & J_LEFT && playerPosition[0] > 8) {
 		playerPosition[0]--;
+        playerDirection = Left;
 	}	
 	
-	// RIGHT
 	if (joypad() & J_RIGHT && playerPosition[0] < 152) {
 		playerPosition[0]++;
+        playerDirection = Right;
 	}
 
     drawPlayer();
 }
 
 void drawPlayer() {
+
+    UINT8 player_dir[4];
+
+    switch (playerDirection)
+    {
+        case Up:
+            memcpy(player_dir, player_up_tile, sizeof player_dir);
+            break;
+        case Right:
+            memcpy(player_dir, player_right_tile, sizeof player_dir);
+            break;
+        case Down:
+            memcpy(player_dir, player_down_tile, sizeof player_dir);
+            break;
+        case Left:
+            memcpy(player_dir, player_left_tile, sizeof player_dir);
+            break;
+    }
+
+    set_sprite_tile(0, player_dir[0]);
+    set_sprite_tile(1, player_dir[1]);
+    set_sprite_tile(2, player_dir[2]);
+    set_sprite_tile(3, player_dir[3]);
+
     move_sprite(0, playerPosition[0], playerPosition[1]);
     move_sprite(1, playerPosition[0] + 8, playerPosition[1]);
     move_sprite(2, playerPosition[0], playerPosition[1] + 8);
